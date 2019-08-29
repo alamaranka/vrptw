06572f54cd0785546302bf187e72154d35853724
Ali P <alip@arutesolutions.com>
2019-08-29 23:37:27 +0300

    appsettings.json added.

---
+++ VRPTW.Configuration/FileOperations.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VRPTW.Configuration
+{
+    class FileOperations
+    {
+        public string FilePath { get; set; }
+        public string FileName { get; set; }
+    }
+}
---
+++ VRPTW.Configuration/SolverParameters.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VRPTW.Configuration
+{
+    class SolverParameters
+    {
+        public double TimeLimit { get; set; }
+        public double MIPGap { get; set; }
+        public int Threads { get; set; }
+    }
+}
