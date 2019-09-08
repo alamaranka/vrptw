1dde77de76df3145e99b66e693c7b2d3673057e3
Ali P <alip@arutesolutions.com>
2019-09-08 22:39:55 +0300

    vehicle added to Route model.

--- VRPTW.Model/Route.cs
+++ VRPTW.Model/Route.cs
@@ -4,6 +4,7 @@ namespace VRPTW.Model
 {
     public class Route
     {
+        public Vehicle Vehicle { get; set; }
         public List<Customer> Customers { get; set; }
         public double Load { get; set; }
         public double Distance { get; set; }
